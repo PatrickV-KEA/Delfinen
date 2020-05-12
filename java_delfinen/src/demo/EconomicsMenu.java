52cc74c7096198c5fd594a97e74df04915468060
Adam Madsen <adam5530@stud.kea.dk>
2020-05-12 15:55:20 +0200

    intellij start

--- java_delfinen/src/demo/EconomicsMenu.java
+++ java_delfinen/src/demo/EconomicsMenu.java
@@ -2,6 +2,16 @@ package demo;
 
 public class EconomicsMenu {
 
-    
+    UI ui = new UI;
+
+
+    public void menu (){
+        /*
+        1. samlede kontinget
+        2. hvem skylder kontingent
+        3. meld en betaling
+         */
+
+    }
 
 }
