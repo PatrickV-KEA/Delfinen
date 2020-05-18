17a29cf59c5dd3a2954a10d7212d8bdd56ea3fe8
kasper fauerby <kasperkbf@hotmail.com>
2020-05-18 13:53:27 +0200

    merged

--- java_delfinen/src/demo/MenuEconomics.java
+++ java_delfinen/src/demo/MenuEconomics.java
@@ -43,7 +43,7 @@ public class MenuEconomics {
                     break;
                 case 4:
                     //indkriv betaling
-                    economics.changePaymentStatus();
+                    //economics.changePaymentStatus();
                     break;
                 case 5:
                     //se samlede kontingent
