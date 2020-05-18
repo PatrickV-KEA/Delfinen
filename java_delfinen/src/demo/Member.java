551ae5ef4afb36953022ff038e203c076d144df2
Patrick Vincent <patrick.vincent@live.dk>
2020-05-18 15:51:07 +0200

    File handling changes, also member.toFile() changes to date formatting

--- java_delfinen/src/demo/Member.java
+++ java_delfinen/src/demo/Member.java
@@ -5,6 +5,7 @@
 package demo;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
 
@@ -17,7 +18,7 @@ public class Member {
     private String      name;
     private LocalDate   birthday;
     private int         cpr;
-    private boolean paid;
+    private boolean     paid;
     private String      discipline;
     private float       bestTime;
     private boolean     active;
@@ -76,7 +77,7 @@ public class Member {
         String str =
                 "id=" + id +
                 " name=" + name.replace(' ', '_') +
-                " date=" + birthday +
+                " date=" + birthday.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")) +
                 " cpr=" + cpr +
                 " paid=" + paid +
                 " active=" + active;
@@ -85,13 +86,12 @@ public class Member {
                 " discipline=" + discipline.replace(' ', '_') +
                 " time=" + bestTime;
         }
-        str += " ¤ ";
         for (int i = 0; i < tournaments.size(); i++) {
-            str +=
-                    "name=" + tournaments.get(i).getName().replace(' ', '_') +
+            str +=  " ¤" +
+                    " name=" + tournaments.get(i).getName().replace(' ', '_') +
                     " result=" + tournaments.get(i).getResult() +
                     " time=" + tournaments.get(i).getTime() +
-                    " date=" + tournaments.get(i).getDate();
+                    " date=" + tournaments.get(i).getDate().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
         }
         return str;
     }
